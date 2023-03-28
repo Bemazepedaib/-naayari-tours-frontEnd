b827166f5019f08fd2f327ff5b27ee0e5d893f34
bemazepedaib <bemazepedaib@ittepic.edu.mx>
2023-03-28 16:43:53 -0700

    added backEnd integration through apollo client

---
+++ src/pages/querys/tripQuerys.js
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+const GET_TRIPS = gql`
+    query getTrips {
+        trips {
+            tripName
+            tripInformation
+            tripKit
+            tripType
+            tripRating
+            tripStatus
+            tripReviews
+        }
+    }
+`
+
+const GET_TRIPTYPES = gql`
+    query getTripTypes {
+        trips {
+            tripName
+            tripType
+        }
+    }
+`;
+
+export { GET_TRIPTYPES }

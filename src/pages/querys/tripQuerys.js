727fa549b2fe9c752b4584136ee3f5724158d8fe
JuanSauced <juan654saga@outlook.com>
2023-03-29 12:19:55 -0700

    Update TripQuerys

--- src/pages/querys/tripQuerys.js
+++ src/pages/querys/tripQuerys.js
@@ -1,18 +1,42 @@
 import { gql } from "@apollo/client";
 
-const GET_TRIPS = gql`
-    query getTrips {
-        trips {
-            tripName
-            tripInformation
-            tripKit
-            tripType
-            tripRating
-            tripStatus
-            tripReviews
-        }
+const GET_TRIPS = gql
+`query getTrips {
+    trips{
+    tripName
+    tripInformation{
+      description
+      place
+      price{
+        priceType
+        priceAmount
+      }
+      discount{
+        dateStart
+        dateEnd
+        amount
+        available
+      }
+      itinerary
+      securityAdvice
+      restrictions
+      recomendations
+      photo
     }
-`
+    tripKit
+    tripType
+    tripRating
+    tripStatus
+    tripReview{
+      user
+      rating
+      review
+      date
+      photo
+    }
+  }
+}
+    `;
 
 const GET_TRIPTYPES = gql`
     query getTripTypes {

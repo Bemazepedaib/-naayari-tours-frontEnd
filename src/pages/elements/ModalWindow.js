76af57cddfcca95556ce2332404b79a3eece4303
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-03-31 10:47:17 -0700

    Preferences Window

---
+++ src/pages/elements/ModalWindow.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Styles from '../../styles/ModalWindow.module.css'
+
+import Image from 'next/image'
+const image = 'https://drive.google.com/uc?export=view&id=1Gx08yGg-rGq0tUe5yVHWxbkaMfmrUOk0'
+
+function ModalWindow({titleText,text,send}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  function activateBothMethods(){
+    handleShow();
+    if(send()){
+      handleClose();
+    }else{
+      handleShow();
+    }
+  }
+
+  return (
+    <>
+      <div className={Styles.buttonWbords} onClick={activateBothMethods}>
+        <p className={Styles.paragraph}>Vamos a viajar!</p>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className={Styles.modalTitle}>
+          <Image className={Styles.image} src={image} width={70} height={70} alt="Naayari tours" />
+            {titleText}
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{text}</Modal.Body>
+        <Modal.Footer className={CenterModalFooter}>
+          <div className={Styles.buttonWbords} onClick={handleClose}>
+            <p>Aceptar</p>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+export default ModalWindow
---
+++ src/pages/sites/Preferences.js
@@ -0,0 +1,67 @@
+import GET_PREFERENCES from './Querys/UserQuerys'
+import { useQuery } from '@apollo/client'
+import Styles from '../../styles/Preferences.module.css'
+import PreferenceCard from '../elements/PreferenceCard'
+import ModalWindow from '../elements/ModalWindow'
+
+const { loading, error, data } = useQuery(GET_PREFERENCES)
+let id = 0
+let preferenceOK = []
+
+function Preferences() {
+    function isOk(name) {
+        preferenceOK.push(name)
+        console.log(preferenceOK[0])
+        console.log(preferenceOK[1])
+        console.log(preferenceOK[2])
+        console.log(preferenceOK[3])
+        console.log(preferenceOK[4])
+        console.log(preferenceOK[5])
+        console.log("----ISOK----")
+    }
+    function isNotOk(name){
+        let pos = preferenceOK.indexOf(name)
+        preferenceOK.splice(pos, 1)
+        console.log(preferenceOK[0])
+        console.log(preferenceOK[1])
+        console.log(preferenceOK[2])
+        console.log(preferenceOK[3])
+        console.log(preferenceOK[4])
+        console.log(preferenceOK[5])
+        console.log("----ISNOT----")
+    }
+    function send(){
+        if(preferenceOK.length === 0){
+            return false;
+        }else{
+            preferenceOK = []
+            return true;
+        }
+    }
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Something Went Wrong</p>
+    return <>
+        {!loading && !error && (
+            <div className={Styles.PreferenceContainer}>
+                <div className={Styles.tittleContainer}>
+                    <h1 className={Styles.tittle}>¿QUE ES LO QUE BUSCAS?
+                        <br/>
+                        <hr/>
+                        ELIGE DE ACUERDO A TUS GUSTOS
+                    </h1>
+                </div>
+                <div className={Styles.imgCards}>
+                    {data.preferences.map(preference => (
+                        <PreferenceCard key={id++} cart={preference} isOk={isOk} isNotOk = {isNotOk} />
+                    ))}
+                </div>
+                <div>
+                    <ModalWindow titleText='Naayari Tours te dice...' 
+                    text='Da clic a una actividad que sea de tu agrado para poder continuar.' send={send}></ModalWindow>
+                </div>
+            </div>
+        )}</>;
+}
+
+
+export default Preferences

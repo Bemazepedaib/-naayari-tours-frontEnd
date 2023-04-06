7c8653981c2dd0b34ee998b5992ead8ff7503b9a
JuanSauced <juan654saga@outlook.com>
2023-04-06 13:55:55 -0600

    Added VIP Buttom on Details Page

--- src/pages/elements/FormTripDate.js
+++ src/pages/elements/FormTripDate.js
@@ -10,20 +10,23 @@ function FormTripDate({ props }) {
         <div>
             <div>
                 <div>
-                    <label> Fechas del viaje </label>
-                    <select
-                        className={Styles.textInput}
-                        value={date}
-                        onChange={e => { setDate(e.target.value) }}
-                        onBlur={e => { setDate(e.target.value) }}>
-                        {props.map(d => (
-                            <option className={Styles.op} value={d} key={d}> {d} </option>
-                        ))}
-                    </select>
+                    <label className={Styles.lbl}> Fechas del viaje </label>
+                    <div className={Styles.cont}>
+
+                        <select
+                            className={Styles.textInput}
+                            value={date}
+                            onChange={e => { setDate(e.target.value) }}
+                            onBlur={e => { setDate(e.target.value) }}>
+                            {props.map(d => (
+                                <option className={Styles.op} value={d} key={d}> {d} </option>
+                            ))}
+                        </select>
+                        <button className={Styles.btn} type='submit' onClick={() => { console.log(date) }}> Reservar lugares </button>
+                    </div>
+                        <button className={Styles.btnVIP} type='submit' onClick={() => { console.log('Es un vip') }}> ¡Crea tu grupo VIP! </button>
                 </div>
-                <div>
-                    <button className={Styles.btn} type='submit' onClick={() => { console.log(date) }}> Reservar lugares </button>
-                </div>
+
             </div>
 
         </div>

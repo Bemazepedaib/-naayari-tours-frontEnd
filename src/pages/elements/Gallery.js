f600b4ebc65ffbd0efd8e2a45fcc0aace125f606
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-10 15:22:17 -0700

    PhotoGallery Method Inherit

--- src/pages/elements/Gallery.js
+++ src/pages/elements/Gallery.js
@@ -3,15 +3,20 @@ import Styles from '../../styles/elementStyles/Gallery.module.css'
 import PhotoGallery from "./PhotoGallery";
 
 
+function getImg(reviews){
+  console.log(reviews)
+}
 function Gallery({ tripReviews }) {
-    let i=0;
+    let i = 0;
     return (
         <div className={Styles.mainBackground}>
             <div className={Styles.reviewSection}>
                 <h2 className={Styles.title}><hr />Experiencias Anteriores<hr /></h2>
                 <div className={Styles.gallery}>
                     {
-                        tripReviews.slice(0,30).map(review => <PhotoGallery key={i++} review={review}></PhotoGallery>)
+                        tripReviews.slice(0,30).map(review =><PhotoGallery key={i++} 
+                            getImg={getImg}
+                            review={review}></PhotoGallery>)
                     }
                 </div>
             </div>
--- src/pages/elements/photoGallery.js
+++ src/pages/elements/photoGallery.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import Styles from '../../styles/elementStyles/PhotoGallery.module.css'
-function PhotoGallery(review) {
-  const [model, setModel] = useState(false);
-  const [tempimgSrc, setTempImgSrc] = useState('');
-  function img(reviews){
-    console.log(reviews)
-  }
+function PhotoGallery({getImg,review}) {
   return (
     <div>
-      <img src={'https://drive.google.com/uc?export=view&id='+review.review.photo}
+      <img src={'https://drive.google.com/uc?export=view&id='+review.photo}
          height={((Math.random() * 10) + 5) * 30} alt='Review Image' className={Styles.image} 
-         onClick={() => img( review.review)}/>
+         onClick={() => getImg(review)}
+         />
     </div>
   )
 }

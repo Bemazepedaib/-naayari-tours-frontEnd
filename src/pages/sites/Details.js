b78245642af0ac54320c0000b5d66773e15dc9bb
JuanSauced <juan654saga@outlook.com>
2023-04-10 14:23:41 -0600

    Added DetailActivity Component

--- src/pages/sites/Details.js
+++ src/pages/sites/Details.js
@@ -7,16 +7,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendarDays, faClock, faTags } from '@fortawesome/free-solid-svg-icons'
 import FormTripDate from "../elements/FormTripDate";
 import Gallery from "../elements/Gallery";
+import DetailActivities from "../elements/DetailActivities";
 
 const Detalles = ({ trip }) => {
 
     console.log(trip)
     const link = "https://drive.google.com/uc?export=view&id="
     const back = link + trip.trip.tripInformation.photo;
-    var tirolesa = "1EAP23WphKNrZnEZz2kLm1g51DHaZ4PUg";
-    var tiro = "1xdUNPdRPW-99dalM7wgirY-Ea6pP103P";
-    var guia = "17YdPzvN4BuYbSpjA5wJxDWq2TyYbO23E";
-    var balneario = "1k_iWaV2734lgOYvmh9CEm-o5B43rtdRw";
+
 
 
 
@@ -52,27 +50,8 @@ const Detalles = ({ trip }) => {
                             </div>
                             <div className={Styles.tourActivities}>
                                 <div className={Styles.header}>Actividades</div>
-                                <div className={Styles.list}>
-                                    <div className={Styles.activities}>
-                                        <Image height={1000}
-                                            width={1000} className={Styles.img} src={link + tirolesa} alt="" />
-                                        <label className={Styles.label}>Tirolesa</label>
-                                    </div>
-                                    <div className={Styles.activities}>
-                                        <Image height={1000}
-                                            width={1000} className={Styles.img} src={link + tiro} alt="" />
-                                        <label className={Styles.label}>Tiro al blanco</label>
-                                    </div>
-                                    <div className={Styles.activities}>
-                                        <Image height={1000}
-                                            width={1000} className={Styles.img} src={link + guia} alt="" />
-                                        <label className={Styles.label}>Guias especializados</label>
-                                    </div>
-                                    <div className={Styles.activities}>
-                                        <Image height={1000}
-                                            width={1000} className={Styles.img} src={link + balneario} alt="" />
-                                        <label className={Styles.label}>Balneario</label>
-                                    </div>
+                                <div >
+                                    <DetailActivities act={trip.trip.tripInformation.activities}></DetailActivities>
                                 </div>
                             </div>
                         </div>

88a46ca8a07390619cf252c55a2ba1e10bf2c91a
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-10 13:52:42 -0700

    Gallery With Google Drive Links Finished

--- src/pages/elements/photoGallery.js
+++ src/pages/elements/photoGallery.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 import Image from 'next/image'
 import Styles from '../../styles/elementStyles/PhotoGallery.module.css'
-function PhotoGallery(photo) {
+function PhotoGallery(review) {
+  const [model, setModel] = useState(false);
+  const [tempimgSrc, setTempImgSrc] = useState('');
+  const getImg = (imgSrc) => {
+      console.log("Esto es una prueba"+imgSrc)
+  }
   return (
     <div>
-      <img src={'https://drive.google.com/uc?export=view&id='+photo.photo}
-         height={((Math.random() * 10) + 5) * 30} alt='Review Image' className={Styles.image}/>
+      <img src={'https://drive.google.com/uc?export=view&id='+review.review.photo}
+         height={((Math.random() * 10) + 5) * 30} alt='Review Image' className={Styles.image} onClick={getImg(review.review.photo)}/>
     </div>
   )
 }

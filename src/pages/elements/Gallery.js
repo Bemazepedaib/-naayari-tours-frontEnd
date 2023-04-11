45860404f03902992946d171ff6088fc0eaf5592
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-11 11:14:59 -0700

    Hook Ref in gallery added

--- src/pages/elements/Gallery.js
+++ src/pages/elements/Gallery.js
@@ -1,26 +1,29 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from 'react';
 import Styles from '../../styles/elementStyles/Gallery.module.css'
 import PhotoGallery from "./PhotoGallery";
 
 
-function getImg(reviews){
-  console.log(reviews)
-}
+
 function Gallery({ tripReviews }) {
+    function getImg(reviews) {
+        console.log(reviews)
+        setImgSelected(true);
+        setReviewSelected(reviews);
+    }
     let i = 0;
-    return (
-        <div className={Styles.mainBackground}>
-            <div className={Styles.reviewSection}>
-                <h2 className={Styles.title}><hr />Experiencias Anteriores<hr /></h2>
-                <div className={Styles.gallery}>
-                    {
-                        tripReviews.slice(0,30).map(review =><PhotoGallery key={i++} 
-                            getImg={getImg}
-                            review={review}></PhotoGallery>)
-                    }
+    const photos = tripReviews.slice(0, 30).map(review => <PhotoGallery key={i++} getImg={getImg} review={review}></PhotoGallery>);
+    const reference = useRef(photos);
+    const [imgSelected, setImgSelected] = useState(false);
+    const [reviewSelected, setReviewSelected] = useState(null);
+        return (
+            <div className={Styles.mainBackground}>
+                <div className={Styles.reviewSection}>
+                    <h2 className={Styles.title}><hr />Experiencias Anteriores<hr /></h2>
+                    <div className={Styles.gallery}>
+                        {reference.current}
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 

9d98839d6a46f3d11f990e919500a81fc102ed78
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-13 09:25:33 -0700

    Card-Information without text

--- src/pages/elements/Gallery.js
+++ src/pages/elements/Gallery.js
@@ -1,29 +1,49 @@
-import React, { useState,useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Styles from '../../styles/elementStyles/Gallery.module.css'
 import PhotoGallery from "./PhotoGallery";
-
-
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Gallery({ tripReviews }) {
-    function getImg(reviews) {
-        console.log(reviews)
-        setImgSelected(true);
-        setReviewSelected(reviews);
-    }
     let i = 0;
     const photos = tripReviews.slice(0, 30).map(review => <PhotoGallery key={i++} getImg={getImg} review={review}></PhotoGallery>);
-    const reference = useRef(photos);
+    const photosGallery = useRef(photos);
     const [imgSelected, setImgSelected] = useState(false);
-    const [reviewSelected, setReviewSelected] = useState(null);
-        return (
+    const reviewInfo = useRef({ photo: '', user: '',rating: '',review: '',date: '' });
+
+    function getImg(reviews) {
+        setImgSelected(true);
+        reviewInfo.current.photo = reviews.photo + ''
+        reviewInfo.current.user = reviews.user + ''
+        reviewInfo.current.rating = reviews.rating + ''
+        reviewInfo.current.date = reviews.date + ''
+        reviewInfo.current.review = reviews.review + ''
+        console.log(reviews)
+    }
+    return (
+        <div className={Styles.mainContainer}>
+            <div className={imgSelected ? Styles.imgOpen : Styles.imgClosed} onClick={() => setImgSelected(false)}>
+                <div className={Styles.cardContainer}>
+                <Image className = {Styles.image}width={500} height={500} alt={'Image Selected'}
+                    src={'https://drive.google.com/uc?export=view&id=' + reviewInfo.current.photo}></Image>
+                 <div className={Styles.cardInformation}>
+                    <h2>{reviewInfo.current.user}</h2>
+                    <p>{reviewInfo.current.review}</p>
+                    <hr className={Styles.horizontalLine}/>
+                 </div>
+                <button className = {Styles.btnClosed}onClick={() => setImgSelected(false)}><FontAwesomeIcon icon={faXmark}></FontAwesomeIcon></button>
+                </div>
+            </div>
             <div className={Styles.mainBackground}>
                 <div className={Styles.reviewSection}>
-                    <h2 className={Styles.title}><hr />Experiencias Anteriores<hr /></h2>
+                    <h2 className={Styles.title}><hr/>Experiencias Anteriores<hr/></h2>
                     <div className={Styles.gallery}>
-                        {reference.current}
+                        {photosGallery.current}
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 

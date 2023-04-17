18d0117598d7c8f50ae11300b4d0bc2a386a5a32
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-17 15:28:16 -0700

    Me design Completed

--- src/pages/sites/Me.js
+++ src/pages/sites/Me.js
@@ -6,15 +6,15 @@ import Styles from '../../styles/Me.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 function Me() {
-    function change(){
+    function change() {
 
     }
     const { loading, error, data } = useQuery(ME);
 
     if (error) return (<div><Navbar />{error.message}</div>)
     if (loading) return (<div><Navbar />Loading...</div>)
-    if (error) return (<div><Navbar/><div className={Styles.errorMe}>{error.message}</div></div>)
-    if (loading) return (<div><Navbar/>Loading...</div>)
+    if (error) return (<div><Navbar /><div className={Styles.errorMe}>{error.message}</div></div>)
+    if (loading) return (<div><Navbar />Loading...</div>)
 
     return <>{!loading && !error &&
         (
@@ -22,19 +22,48 @@ function Me() {
                 <Navbar></Navbar>
                 <div className={Styles.mainContainer}>
                     <div className={Styles.titlesContainer}>
-                        <h1 className={Styles.titleCount}>Cuenta</h1>
-                        <h2 className={Styles.titleChanges}>Edita la configuración de tu cuenta 
-                        y cambia la contraseña aquí.</h2>
+                        <h1 className={Styles.titleCount}>Cuenta</h1><hr />
+                        <h2 className={Styles.titleChanges}>Edita la configuración de tu cuenta
+                            y cambia la contraseña aquí.</h2>
                         {console.log(data.me.name)}
                     </div>
-                    <div className={Styles.emailContainer}>
-                        <h2 className={Styles.titleEmail}>correo Electrónico</h2><hr/>
-                        <div className={Styles.emailDataContainer}>
-                            <p className={Styles.emailData}>
-                               <b>{data.me.email}</b>
+                    <div className={Styles.dataMainContainer}>
+                        <h2 className={Styles.titleData}>Datos personales</h2><hr />
+                        <h3 className={Styles.subtitleData}>Cambiar Nombre</h3>
+                        <div className={Styles.dataContainer}>
+                            <p className={Styles.data}>
+                                {data.me.name}
                             </p>
                             <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
                         </div>
+                        <h3 className={Styles.subtitleData}>Cambiar Télefono</h3>
+                        <div className={Styles.dataContainer}>
+                            <p className={Styles.data}>
+                                {data.me.cellphone}
+                            </p>
+                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                        </div>
+                        <h3 className={Styles.subtitleData}>Fecha de nacimiento</h3>
+                        <div className={Styles.dataContainer}>
+                            <p className={Styles.data}>
+                                {data.me.birthDate}
+                            </p>
+                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                        </div>
+                        <div className={Styles.btnChangePassContainer}>
+                            <button className={Styles.btnChangePass}>Cambiar las preferencias</button>
+                        </div>
+                    </div>
+                    <div className={Styles.passContainer}>
+                        <h2 className={Styles.titlePass}>Contraseña</h2><hr />
+                        <form className={Styles.inputContainer}>
+                            <input className={Styles.input} type="password" placeholder='Introducir contraseña actual'></input>
+                            <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña'></input>
+                            <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña de nuevo'></input>
+                            <div className={Styles.btnChangePassContainer}>
+                                <button className={Styles.btnChangePass}>Cambiar La Contraseña</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>

c5a9a3826a7a8fc4e1946d60b6255b7da8382b18
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-17 12:40:11 -0700

    Me with the design started

--- src/pages/sites/Me.js
+++ src/pages/sites/Me.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ME } from '../querys/userQuerys';
 import Navbar from './Navbar';
 import Styles from '../../styles/Me.module.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 function Me() {
+    function change(){
 
+    }
     const { loading, error, data } = useQuery(ME);
 
-    if (error) return (<div><Navbar/>{error.message}</div>)
-    if (loading) return (<div><Navbar/>Loading...</div>)
+    if (error) return (<div><Navbar />{error.message}</div>)
+    if (loading) return (<div><Navbar />Loading...</div>)
 
     return <>{!loading && !error &&
         (
             <div>
                 <Navbar></Navbar>
-                <div>
-                    {console.log(data)}
+                <div className={Styles.mainContainer}>
+                    <div className={Styles.titlesContainer}>
+                        <h1 className={Styles.titleCount}>Cuenta</h1>
+                        <h2 className={Styles.titleChanges}>Edita la configuración de tu cuenta 
+                        y cambia la contraseña aquí.</h2>
+                        {console.log(data.me.name)}
+                    </div>
+                    <div className={Styles.emailContainer}>
+                        <h2 className={Styles.titleEmail}>correo Electrónico</h2><hr/>
+                        <div className={Styles.emailDataContainer}>
+                            <p className={Styles.emailData}>
+                               <b>{data.me.email}</b>
+                            </p>
+                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )

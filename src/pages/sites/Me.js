e72b90d160d72308932da4dd9862e73613f01bf9
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-17 16:34:13 -0700

    Me with modal included

--- src/pages/sites/Me.js
+++ src/pages/sites/Me.js
@@ -5,10 +5,13 @@ import Navbar from './Navbar';
 import Styles from '../../styles/Me.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 function Me() {
-    function change() {
-
-    }
+    const [show, setShow] = useState(false);
+    console.log(show);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const { loading, error, data } = useQuery(ME);
 
     if (error) return (<div><Navbar />{error.message}</div>)
@@ -22,50 +25,64 @@ function Me() {
                 <Navbar></Navbar>
                 <div className={Styles.mainContainer}>
                     <div className={Styles.titlesContainer}>
-                        <h1 className={Styles.titleCount}>Cuenta</h1><hr />
-                        <h2 className={Styles.titleChanges}>Edita la configuración de tu cuenta
-                            y cambia la contraseña aquí.</h2>
+                        <h1 className={Styles.titleCount}>CUENTA</h1><hr />
+                        <h2 className={Styles.titleChanges}>EDITA LOS DATOS PERSONALES DE TU CUENTA Y
+                            CAMBIA LA CONTRASEÑA AQUÍ</h2>
                         {console.log(data.me.name)}
                     </div>
                     <div className={Styles.dataMainContainer}>
-                        <h2 className={Styles.titleData}>Datos personales</h2><hr />
-                        <h3 className={Styles.subtitleData}>Cambiar Nombre</h3>
+                        <h2 className={Styles.titleData}>DATOS PERSONALES</h2><hr />
+                        <h3 className={Styles.subtitleData}>CAMBIAR NOMBRE</h3>
                         <div className={Styles.dataContainer}>
                             <p className={Styles.data}>
                                 {data.me.name}
                             </p>
-                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                            <button onClick={handleShow}  className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare}/></button>
                         </div>
-                        <h3 className={Styles.subtitleData}>Cambiar Télefono</h3>
+                        <h3 className={Styles.subtitleData}>CAMBIAR TÉLEFONO</h3>
                         <div className={Styles.dataContainer}>
                             <p className={Styles.data}>
                                 {data.me.cellphone}
                             </p>
-                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                            <button onClick={handleShow} className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} /></button>
                         </div>
-                        <h3 className={Styles.subtitleData}>Fecha de nacimiento</h3>
+                        <h3 className={Styles.subtitleData}>FECHA DE NACIMIENTO</h3>
                         <div className={Styles.dataContainer}>
                             <p className={Styles.data}>
                                 {data.me.birthDate}
                             </p>
-                            <button className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} onClick={change} /></button>
+                            <button onClick={handleShow} className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} /></button>
                         </div>
                         <div className={Styles.btnContainer}>
-                            <button className={Styles.btnChange}>Cambiar las preferencias</button>
+                            <button className={Styles.btnChange}>CAMBIAR LAS PREFERENCIAS</button>
                         </div>
                     </div>
                     <div className={Styles.passContainer}>
-                        <h2 className={Styles.titlePass}>Contraseña</h2><hr />
+                        <h2 className={Styles.titlePass}>CONTRASEÑA</h2><hr/>
                         <form className={Styles.inputContainer}>
                             <input className={Styles.input} type="password" placeholder='Introducir contraseña actual'></input>
                             <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña'></input>
                             <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña de nuevo'></input>
                             <div className={Styles.btnContainer}>
-                                <button className={Styles.btnChange}>Cambiar La Contraseña</button>
+                                <button className={Styles.btnChange}>CAMBIAR LA CONTRASEÑA</button>
                             </div>
                         </form>
                     </div>
                 </div>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }</>;

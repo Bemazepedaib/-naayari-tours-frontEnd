dfc3c32f85165c2c185dcd1abad7a2ccca1f8055
JuanSauced <juan654saga@outlook.com>
2023-04-17 12:50:53 -0600

    Redesign at the Navbar

--- src/pages/sites/Navbar.js
+++ src/pages/sites/Navbar.js
@@ -23,27 +23,33 @@ function Navbar() {
         <div className={Styles.navbarHeader}>
             <Link href="/">
                 <Image src={image}
-                    width={100}
-                    height={100}
+                    width={55}
+                    height={55}
                     alt="Naayari Tours"
                 />
             </Link>
             <nav className={Styles.navbar} ref={navRef}>
-                <Link className={Styles.link} href="/">Inicio</Link>
-                <Link className={Styles.link} href="/sites/AboutUs">Acerca de Nosotros</Link>
-                <Link className={Styles.link} href="/sites/FAQ">Preguntas Frecuentes</Link>
-                <Link className={Styles.link} href="#Footer" scroll={false} >Contacto</Link>
-                <Link className={Styles.link} href="/sites/Me">Mi perfil</Link>
-                <Link href="/sites/Login"><button className={Styles.btnLogin}>Iniciar Sesión</button></Link>
-                <Link href=""><button className={Styles.btnLogin} onClick={() => {
-                    localStorage.removeItem('token');
-                    window.location.reload(true)
-                }}>Cerrar Sesión</button></Link>
-                <button
-                    className={`${Styles.navBtn} ${Styles.navCBtn}`}
-                    onClick={showNavbar}>
-                    <FontAwesomeIcon icon={faX} />
-                </button>
+                <div className={Styles.Links}>
+                    <Link className={Styles.link} href="/">Inicio</Link>
+                    <Link className={Styles.link} href="/sites/AboutUs">Acerca de Nosotros</Link>
+                    <Link className={Styles.link} href="/sites/FAQ">Preguntas Frecuentes</Link>
+                    <Link className={Styles.link} href="#Footer" scroll={false} >Contacto</Link>
+                    <Link className={Styles.link} href="/sites/Me">Mi perfil</Link>
+                </div>
+                <div className={Styles.buttons}>
+
+
+                    <Link href="/sites/Login"><button className={Styles.btnLogin}>Iniciar Sesión</button></Link>
+                    <Link href=""><button className={Styles.btnLogin} onClick={() => {
+                        localStorage.removeItem('token');
+                        window.location.reload(true)
+                    }}>Cerrar Sesión</button></Link>
+                    <button
+                        className={`${Styles.navBtn} ${Styles.navCBtn}`}
+                        onClick={showNavbar}>
+                        <FontAwesomeIcon icon={faX} />
+                    </button>
+                </div>
             </nav>
             <button
                 className={Styles.navBtn}

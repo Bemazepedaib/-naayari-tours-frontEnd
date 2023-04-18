96f7e423494433bec7d6d4cce8a3c6e9394f232e
JuanSauced <juan654saga@outlook.com>
2023-04-18 14:20:03 -0600

    Some changes on Navbar

--- src/pages/sites/UpdateUser.js
+++ src/pages/sites/UpdateUser.js
@@ -1,29 +1,41 @@
 import React from 'react'
 
 import Styles from '../../styles/elementStyles/UpdateUser.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 function UpdateUser({ user }) {
   console.log(user)
   return (
     <div className={Styles.mainContainer}>
       <div className={Styles.info}>
-        <p> Nombre de usuario: {user.user.name} </p>
-        <p> Tipo de usuario: {user.user.userType}</p>
-        <p> Telefono: {user.user.cellphone} </p>
-        <p> Correo: {user.user.email} </p>
-        <p> Fecha de nacimiento: {user.user.birthDate}</p>
-        <p> Nivel de usuario: {user.user.userLevel}</p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Nombre de usuario:   </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.name}       
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Telefono:            </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.cellphone}  
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Correo:              </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.email}      
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Tipo de usuario:     </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.userType}   
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Fecha de nacimiento: </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.birthDate}  
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
+        <p className={`${Styles.infoP} ${Styles.infoSubtitle}`}> Nivel de usuario:    </p>
+        <p className={`${Styles.infoP} ${Styles.update}`}>{user.user.userLevel}  
+        <i className={Styles.icon}><FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon></i></p>
         <div>
-        Contraseña nueva: <input></input>
-      </div>
+          Contraseña nueva: <input></input>
+        </div>
       </div>
       <div className={Styles.tags}>
-      <h3 className={Styles.tagsInfo}>Categorias</h3>
-      </div>
-      <div className={Styles.reviews}>
-        <h3 className={Styles.reviewsInfo}>Reviews</h3>
+        <p className={`${Styles.tagsInfo} ${Styles.infoSubtitle}`}> Preferencias </p>
 
       </div>
+ 
 
     </div>
   )

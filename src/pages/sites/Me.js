808232025b9bf957a8a6a10898c212dadf91b876
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-04-26 13:04:17 -0700

    I added more user mutations and me finished

--- src/pages/sites/Me.js
+++ src/pages/sites/Me.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { ME } from '../querys/userQuerys';
 import Navbar from './Navbar';
 import Styles from '../../styles/Me.module.css'
@@ -9,9 +9,23 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Image from 'next/image'
 
+import { UPDATE_USER_NAME, UPDATE_USER_CELL, UPDATE_USER_PASSWORD } from '../mutations/userMutations';
+
 import Router from 'next/router';
 
 function Me() {
+    const [userName] = useMutation(UPDATE_USER_NAME);
+    const [userCell] = useMutation(UPDATE_USER_CELL);
+    const [userPass] = useMutation(UPDATE_USER_PASSWORD);
+    const onChange = (e) => {
+        if(e.target.name === "value"){
+            setUpdateValue(e.target.value)
+        }else if(e.target.name === "confirmValue"){
+            setConfirmValue(e.target.value)
+        }else{
+            setPassWord(e.target.value)
+        }
+    }
     const initialValue = {
         title: '',
         value: '',
@@ -19,29 +33,32 @@ function Me() {
     }
     const image = 'https://drive.google.com/uc?export=view&id=1Gx08yGg-rGq0tUe5yVHWxbkaMfmrUOk0'
     const [info, setInfo] = useState(initialValue);
+    const [updateValue, setUpdateValue] = useState('');
+    const [password, setPassWord] = useState("");
+    const [confirmValue, setConfirmValue] = useState("");
     const handleClose = () => setShow(false);
     const [show, setShow] = useState(false);
     const goPreferences = () => {
         Router.push({ pathname: '../elements/MePreferences' })
     }
-    const changeData = () => {
+    const changeData = async () => {
         handleClose()
         switch (info.data) {
             case "name":
-
+                await userName({ variables: { newName: updateValue, password: password } })
                 break;
             case "telefono":
-
+                await userCell({ variables: { newCell: updateValue, password: password } })
                 break;
-            case "birthDate":
-
-                break;
-
         }
     }
-    const changePassword = (e) => {
+    const changePassword = async (e) => {
         e.preventDefault();
-        console.log("CACA")
+        console.log(updateValue)
+        console.log(confirmValue)
+        if(updateValue === confirmValue){
+            await userPass({ variables: { newPassword: updateValue, password: password } });
+        }
     }
     const handleShow = (pressedButton) => {
         setInfo(initialValue)
@@ -55,6 +72,7 @@ function Me() {
                     data: 'name'
                 }
                 setInfo(value1)
+                setUpdateValue(data.me.name + "")
                 break;
             case "telefono":
                 const value2 = {
@@ -65,16 +83,7 @@ function Me() {
                     data: 'telefono'
                 }
                 setInfo(value2)
-                break;
-            case "birthDate":
-                const value3 = {
-                    title: 'Cambia tu fecha de nacimiento',
-                    value: data.me.birthDate,
-                    type: 'date',
-                    message: '',
-                    data: 'birthDate'
-                }
-                setInfo(value3)
+                setUpdateValue(data.me.cellphone)
                 break;
         }
         setShow(true);
@@ -110,24 +119,26 @@ function Me() {
                             </p>
                             <button onClick={() => handleShow('telefono')} className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} /></button>
                         </div>
-                        <h3 className={Styles.subtitleData}>FECHA DE NACIMIENTO</h3>
-                        <div className={Styles.dataContainer}>
-                            <p className={Styles.data}>
-                                {data.me.birthDate}
-                            </p>
-                            <button onClick={() => handleShow('birthDate')} className={Styles.btnEdit}> <FontAwesomeIcon icon={faPenToSquare} /></button>
-                        </div>
                         <div className={Styles.btnContainer}>
                             <button className={Styles.btnChange} onClick={() => goPreferences()}>CAMBIAR LAS PREFERENCIAS</button>
                         </div>
                     </div>
                     <div className={Styles.passContainer}>
                         <form className={Styles.inputContainer} onSubmit={changePassword}>
-                            <h2 className={Styles.titlePass} name='password'>CONTRASEÑA</h2>
+                            <h2 className={Styles.titlePass}>CONTRASEÑA</h2>
                             <hr className={Styles.hr} />
-                            <input className={Styles.input} type="password" placeholder='Introducir contraseña actual'></input>
-                            <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña'></input>
-                            <input className={Styles.input} type="password" placeholder='Introducir nueva contraseña de nuevo'></input>
+                            <input className={Styles.input} value={password} onChange={onChange} 
+                            type="password" placeholder='Introducir contraseña actual'>
+
+                            </input>
+                            <input className={Styles.input} value={updateValue} onChange={onChange} 
+                            type="password" placeholder='Introducir nueva contraseña' name='value'>
+
+                            </input>
+                            <input className={Styles.input} value={confirmValue} onChange={onChange} 
+                            type="password" placeholder='Introducir nueva contraseña de nuevo' name='confirmValue'>
+
+                            </input>
                             <div className={Styles.btnContainer}>
                                 <button type="submit" className={Styles.btnChange}>CAMBIAR LA CONTRASEÑA</button>
                             </div>
@@ -142,14 +153,14 @@ function Me() {
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body className={Styles.modalBody}>
-                        <form>
-                            <input className={Styles.input} type={info.type} placeholder={info.message}
-                                defaultValue={info.value} required>
-                            </input>
-                            <input className={Styles.input} type="password"
-                                placeholder='Introduce tu contraseña' required>
-                            </input>
-                        </form>
+                        <input className={Styles.input} type={info.type} placeholder={info.message}
+                            value={updateValue} name='value'
+                            onChange={onChange} required>
+                        </input>
+                        <input className={Styles.input} type="password"
+                            placeholder='Introduce tu contraseña' value={password}
+                            onChange={onChange} required>
+                        </input>
                     </Modal.Body>
                     <Modal.Footer className={Styles.modalFooter}>
                         <Button className={Styles.btnSave} variant="btn btn-dark" onClick={() => changeData()}>

2d7d20ecbf345af2b946999ae3e950e0c84bc9ec
JuanSauced <juan654saga@outlook.com>
2023-05-15 12:25:31 -0700

    Added Modal at the SearchBar component

---
+++ src/pages/elements/ModalUsers.js
@@ -0,0 +1,86 @@
+//IMPORTS
+import React, { useState } from 'react';
+import Styles from '../../styles/elementStyles/ModalTrips.module.css'
+import Router from 'next/router';
+
+//MUTATIONS
+import { useMutation } from '@apollo/client';
+import { UPDATE_TRIP_STATUS } from '../mutations/tripMutations';
+
+//COMPONENTS
+import Modal from 'react-bootstrap/Modal';
+import Image from 'next/image'
+
+//ICONS
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+const ModalUsers = ({userData}) => {
+    //MODAL NUMBER 1
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    //MODAL NUMBER 2
+    const [show1, setShow1] = useState(false);
+    const handleClose1 = () => setShow1(false);
+    const handleCloseConfirm = async () => { 
+        console.log("Eliminar usuario")
+    }
+    const handleShow1 = () => {setShow1(true); setShow(false);}
+    //MUTATION USE
+    const [updateTripStatus] = useMutation(UPDATE_TRIP_STATUS);
+    //NAAYARI TOURS IMAGE
+    const image = 'https://drive.google.com/uc?export=view&id=1Gx08yGg-rGq0tUe5yVHWxbkaMfmrUOk0'
+
+    const email=userData.email;
+    function updateUser() {
+        console.log(userData)
+        Router.push({
+            pathname: '../elements/User',
+            query: { email }
+        })
+    } 
+
+    return (
+        <div>
+            {/*BUTTON TO OPEN MODAL 1*/}
+            <div className={Styles.buttonIcon}>
+            <i className={Styles.icon} onClick={handleShow}><FontAwesomeIcon icon={faPenToSquare} ></FontAwesomeIcon></i>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title className={Styles.modalTitle}>
+                        <Image className={Styles.image} src={image} width={100} height={100} alt="Naayari tours" />
+                        ¿QUE ACCIÓN QUIERES HACER?
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className={Styles.modalBody}>El usuario seleccionado es: {userData.name}<br />
+                    Con correo electronico:{userData.email}
+                </Modal.Body>
+                <Modal.Footer className={Styles.modalFooter}>
+                    <button className={Styles.btnDelete} onClick={handleShow1 }>Eliminar</button>
+                    <button className={Styles.btnUpdate} onClick={updateUser}>Actualizar</button>
+                </Modal.Footer>
+            </Modal>
+            {/*MODAL 2*/}
+            <Modal show={show1} onHide={handleClose1}>
+                <Modal.Header closeButton>
+                    <Modal.Title className={Styles.modalTitle}>
+                        <Image className={Styles.image} src={image} width={100} height={100} alt="Naayari tours" />
+                        CONFIRMAR CAMBIO
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className={Styles.modalBody}>
+                    ¡ESTE USUARIO SE ELIMINARÁ DEL SISTEMA!
+                </Modal.Body>
+                <Modal.Footer className={Styles.modalFooter}>
+                    <button className={Styles.btnCancel} onClick={handleClose1}>NO</button>
+                    <button className={Styles.btnConfirm} onClick={handleCloseConfirm}>SÍ</button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+
+export default ModalUsers
--- src/pages/sites/UpdateUser.js
+++ src/pages/sites/UpdateUser.js
@@ -9,16 +9,13 @@ function UpdateUser({ user }) {
 
   const [newName, setNewName] = useState("");
 
-  const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    password: /^.{4,12}$/, // 4 a 12 digitos.
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-  }
+
 
 
   return (
+    
     <div className={Styles.mainContainer}>
+      {console.log(user)}
       <div className={Styles.info}>
         <section  className={Styles.section}>
         <p className={` ${Styles.infoSubtitle}`}> Nombre de usuario:   </p>

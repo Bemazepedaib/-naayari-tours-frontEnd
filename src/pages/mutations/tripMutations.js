a8b8ee9c34302ab81c6705fbd6ea0f75abff9f30
GustavoMaLem30 <gustavoml2097@hotmail.com>
2023-05-17 13:24:43 -0700

    Update trip information completed

--- src/pages/mutations/tripMutations.js
+++ src/pages/mutations/tripMutations.js
@@ -17,4 +17,11 @@ const UPDATE_TRIP_STATUS = gql`
         updateTripStatus(tripName: $tripName,tripStatus: $tripStatus)
     }
 `;
-export { ADD_TRIP,DELETE_TRIP,UPDATE_TRIP_STATUS}
+const UPDATE_TRIP = gql`
+    mutation updateTrip($tripName: String!,$tripInformation: InputTripInformation!,
+        $tripKit: String!,$tripRating: Int!,$tripStatus: Boolean!, $tripReview: [InputTripReview!]){
+            updateTrip(tripName: $tripName,tripInformation:$tripInformation,
+                tripKit:$tripKit,tripRating:$tripRating,tripStatus:$tripStatus,tripReview:$tripReview)
+        }
+`;
+export { ADD_TRIP,DELETE_TRIP,UPDATE_TRIP_STATUS,UPDATE_TRIP}

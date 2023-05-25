c9498bc30b89a301b9d9ba58bfff5bd78e60a0b9
bemazepedaib <benjaminzepedaibarra@gmail.com>
2023-05-24 18:58:17 -0700

    added login button to sidebarAdmin

--- src/pages/elements/AdminDashboard/SidebarAdmin.js
+++ src/pages/elements/AdminDashboard/SidebarAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Styles from '../../../styles/elementStyles/SidebarAdmin.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlane, faUser, faArrowsDownToPeople, faHome, faPowerOff } from '@fortawesome/free-solid-svg-icons'
@@ -6,9 +6,29 @@ import { faPlane, faUser, faArrowsDownToPeople, faHome, faPowerOff } from '@fort
 import Link from 'next/link'
 import Image from 'next/image'
 
+function checkForToken() {
+
+    const [login, setLogin] = useState()
+    useEffect(() => {
+        setLogin(localStorage.getItem("token"))
+    }, [])
+
+    return [login, setLogin]
+}
+
 function SidebarAdmin() {
 
     const image = 'https://drive.google.com/uc?export=view&id=1hKQxSheX5io9bPjn99_TedN8SCTNcsoK'
+
+    const [login, setLogin] = checkForToken()
+
+    useEffect(() => {
+        if (localStorage.getItem("token") !== null) {
+            setLogin("token")
+        } else {
+            setLogin("not token")
+        }
+    }, [])
 
     return (
         <div className={Styles.sidebar} id={Styles.sidebarAdmin}>
@@ -23,28 +43,34 @@ function SidebarAdmin() {
             </div>
             <div className={Styles.sidebarAdminMenu}>
                 <Link href="/sites/Dashboard" className={Styles.sidebarAdminLink}>
-                    <FontAwesomeIcon icon={faHome} /> 
+                    <FontAwesomeIcon icon={faHome} />
                     <div className={Styles.text}>Dashboard</div>
                 </Link>
                 <Link href="/sites/TripView" className={Styles.sidebarAdminLink}>
-                    <FontAwesomeIcon icon={faPlane} /> 
+                    <FontAwesomeIcon icon={faPlane} />
                     <div className={Styles.text}>Viajes</div>
                 </Link>
                 <Link href="/sites/Users" className={Styles.sidebarAdminLink}>
-                    <FontAwesomeIcon icon={faUser} /> 
+                    <FontAwesomeIcon icon={faUser} />
                     <div className={Styles.text}>Usuarios</div>
                 </Link>
                 <Link href="/sites/Events" className={Styles.sidebarAdminLink}>
-                    <FontAwesomeIcon icon={faArrowsDownToPeople} /> 
+                    <FontAwesomeIcon icon={faArrowsDownToPeople} />
                     <div className={Styles.text}>Org. de Viajes</div>
                 </Link>
-                <Link href="/" className={Styles.sidebarAdminLogout} onClick={() => {
-                    localStorage.removeItem('token');
-                    window.location.reload(true)
-                }}>
-                    <FontAwesomeIcon icon={faPowerOff} />
-                    <div className={Styles.text}>Cerrar Sesión</div>
-                </Link>
+                {login === 'token' ?
+                    <Link href="/" className={Styles.sidebarAdminLogout} onClick={() => {
+                        localStorage.removeItem('token');
+                        window.location.reload(true)
+                    }}>
+                        <FontAwesomeIcon icon={faPowerOff} />
+                        <div className={Styles.text}>Cerrar Sesión</div>
+                    </Link> :
+                    <Link href="/sites/Login" className={Styles.sidebarAdminLogout}>
+                        <FontAwesomeIcon icon={faPowerOff} />
+                        <div className={Styles.text}>Iniciar Sesión</div>
+                    </Link>
+                }
             </div>
         </div>
     )

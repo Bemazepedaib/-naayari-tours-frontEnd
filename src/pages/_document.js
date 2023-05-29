7c8f502eb3397a1cbc0420ec21cdc5822a27a2cf
bemazepedaib <benjaminzepedaibarra@gmail.com>
2023-05-29 12:13:10 -0700

    added fonts to head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@1,700&family=Oswald:wght@300&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -12,6 +15,7 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
+      <title>Naayari tours</title>
       <body>
         <Main />
         <NextScript />

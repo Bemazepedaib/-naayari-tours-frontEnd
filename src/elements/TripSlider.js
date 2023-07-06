b5801d38a903d970c3335dbfc4d88c69dca4a893
bemazepedaib <benjaminzepedaibarra@gmail.com>
2023-07-06 11:11:32 -0700

    tripSlider bug fix try 1

--- src/elements/TripSlider.js
+++ src/elements/TripSlider.js
@@ -9,91 +9,88 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const TripSlider = ({ title, preferencesData }) => {
-  const sliderRef = useRef(null)
-  let a = 0;
-  var variable = {
-    infinite: true,
-    slidesToShow: window.innerWidth <= 1080 ? 1 : preferencesData.length > 3 ? 3 : preferencesData.length,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: true,
-    arrows: false
-  };
-  const [settings, setSettings] = useState(variable);
-  const goToDetailCard = (name) => {
-    Router.push({
-      pathname: '/DetailCard',
-      query: { name }
-    })
-  }
-  useEffect(() => {
-    const handleResize = () => {
-      var variable = {
-        infinite: true,
-        slidesToShow: window.innerWidth <= 1080 ? 1 : preferencesData.length > 3 ? 3 : preferencesData.length,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        pauseOnHover: true,
-        arrows: false
-      };
-      setSettings(variable)
+	const sliderRef = useRef(null)
+	let a = 0;
+	var variable = {
+		infinite: true,
+		slidesToShow: window.innerWidth <= 1080 ? 1 : preferencesData.length > 3 ? 3 : preferencesData.length,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		pauseOnHover: true,
+		arrows: false
+	};
+	const [settings, setSettings] = useState(variable);
+	const goToDetailCard = (name) => {
+		Router.push({
+			pathname: '/DetailCard',
+			query: { name }
+		})
+	}
+	useEffect(() => {
+		const handleResize = () => {
+			var variable = {
+				infinite: true,
+				slidesToShow: window.innerWidth <= 1080 ? 1 : preferencesData.length > 3 ? 3 : preferencesData.length,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 4000,
+				pauseOnHover: true,
+				arrows: false
+			};
+			setSettings(variable)
 
-    };
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	
+	return (
+		<div>
+			<div className={Styles.header}>
+				<h2 className={Styles.headerTitle}>{title}</h2>
+			</div>
+			<div className={Styles.container}>
+				<div className={Styles.buttons}>
+					<div className={Styles.buttonBack} onClick={() => sliderRef.current.slickPrev()}>
+						<FontAwesomeIcon icon={faArrowLeft} />
+					</div>
+				</div>
+				<div className={Styles.sliderContainer}>
+					<Slider ref={sliderRef} {...settings} className={Styles.prueba}>
+						{
+							preferencesData.map(preference => (
+								<div className={Styles.cardsContainer} key={a++}>
+									<div className={Styles.imageContainer} onClick={() => goToDetailCard(preference.tripName)}>
+										<img className={Styles.imageCa}
+											src={'https://drive.google.com/uc?export=view&id=' + preference.tripInformation.photo} alt=''>
+										</img>
+										<div className={Styles.overlayTitle}>
+											<h3 className={Styles.title}>{preference.tripName}</h3>
+										</div>
+										<div className={Styles.overlayInfo}>
+											<h3 className={Styles.title}>{preference.tripName}</h3>
+											<p className={Styles.catpd}>
+												{preference.tripInformation.description.slice(0, 200)}...
+											</p>
+										</div>
+									</div>
+								</div>
 
-    // Agregar el evento 'resize' al cargar el componente
-    window.addEventListener('resize', handleResize);
-
-    // Eliminar el evento 'resize' al desmontar el componente
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  return (
-    <div>
-      <div className={Styles.header}>
-        <h2 className={Styles.headerTitle}>{title}</h2>
-      </div>
-      <div className={Styles.container}>
-        <div className={Styles.buttons}>
-          <div className={Styles.buttonBack} onClick={() => sliderRef.current.slickPrev()}>
-            <FontAwesomeIcon icon={faArrowLeft} />
-          </div>
-        </div>
-        <div className={Styles.sliderContainer}>
-          <Slider ref={sliderRef} {...settings} className={Styles.prueba}>
-            {
-              preferencesData.map(preference => (
-                <div className={Styles.cardsContainer} key={a++}>
-                  <div className={Styles.imageContainer} onClick={() => goToDetailCard(preference.tripName)}>
-                    <img className={Styles.imageCa}
-                      src={'https://drive.google.com/uc?export=view&id=' + preference.tripInformation.photo} alt=''>
-                    </img>
-                    <div className={Styles.overlayTitle}>
-                      <h3 className={Styles.title}>{preference.tripName}</h3>
-                    </div>
-                    <div className={Styles.overlayInfo}>
-                      <h3 className={Styles.title}>{preference.tripName}</h3>
-                      <p className={Styles.catpd}>
-                        {preference.tripInformation.description.slice(0, 200)}...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-              ))
-            }
-          </Slider>
-        </div>
-        <div className={Styles.buttons}>
-          <div className={Styles.buttonNext} onClick={() => sliderRef.current.slickNext()}>
-            <FontAwesomeIcon icon={faArrowRight} />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+							))
+						}
+					</Slider>
+				</div>
+				<div className={Styles.buttons}>
+					<div className={Styles.buttonNext} onClick={() => sliderRef.current.slickNext()}>
+						<FontAwesomeIcon icon={faArrowRight} />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default TripSlider
